4169fba606d28797b53f7eacd5dfb85474679307
akshay kalucha <akshaykalucha@gmail.com>
2021-11-06 12:10:51 +0530

    equalcoin

--- Codechef/November-Long/equalcoin.py
+++ Codechef/November-Long/equalcoin.py
@@ -1,6 +1,7 @@
 for _ in range(int(input())):
     X, Y = map(int, input().split())
-    if X%2==0 and Y%2==0:
+    two = Y*2
+    if (X+two) % 2 == 0:
         print("YES")
     else:
         print("NO")
