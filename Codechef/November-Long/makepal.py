0db4461857677f8b4a3dd4993621c5c3cb653a94
akshay kalucha <akshaykalucha@gmail.com>
2021-11-08 12:15:40 +0530

    makepalsolution

--- Codechef/November-Long/makepal.py
+++ Codechef/November-Long/makepal.py
@@ -2,15 +2,15 @@ for _ in range(int(input())):
     n = int(input())
     ll = list(map(int, input().split()))
     odd = 0
-    if len(ll) <= 2:
+    for num in ll:
+        if num%2!=0:
+            odd += 1
+    if odd == 0 or odd == 1:
         print(0)
+    elif odd == 2:
+        print(1)
     else:
-        for num in ll:
-            if num%2!=0:
-                odd += 1
-        if odd == 0 or odd == 1:
-            print(0)
-        elif odd == 2:
-            print(1)
+        if odd%2==0:
+            print(odd//2)
         else:
             print((odd-1)//2)
