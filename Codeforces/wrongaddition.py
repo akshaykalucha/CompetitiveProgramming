67cf28e48cd0c66f675cd874c0fc141a673545b2
akshay kalucha <akshaykalucha@gmail.com>
2021-12-20 22:46:39 +0530

    wrong addiion

---
+++ Codeforces/wrongaddition.py
@@ -0,0 +1,32 @@
+def find(a,s):
+    i = len(s) -1
+    j = len(a) -1
+    
+    res = ''
+    c = 0
+    while i >-1 and j >-1:
+        if int(s[i]) >= int(a[j]) and int(s[i]) - int(a[j]) <10:
+            res += str(int(s[i]) - int(a[j]))
+            i -= 1
+            j -= 1
+        elif i > 0 and int(s[i-1:i+1]) > int(a[j]) and int(s[i-1:i+1]) - int(a[j]) <10:
+            res += str(int(s[i-1:i+1]) - int(a[j]))
+            i -= 2
+            j -= 1
+            c += 1
+        else:
+            return -1
+    if i >= j:
+        return int(s[:i+1] + res[::-1])
+    else:
+        return -1
+            
+ 
+r = int(input())
+res = []
+for i in range(r):
+    arr = input().split()
+    res.append(find(arr[0], arr[1]))
+    
+for i in res:
+    print(i)
