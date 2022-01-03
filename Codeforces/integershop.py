5ae5a34ef2c4b0cb79d619d3f385d51533547265
akshay kalucha <akshaykalucha@gmail.com>
2022-01-03 21:43:57 +0530

    add

---
+++ Codeforces/integershop.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdout = open('DSA/Stacks/output.txt', 'w')
+sys.stdin = open('DSA/Stacks/input.txt', 'r')
+for _ in range(int(input())):
+    n = int(input())
+    cmin = 9999999999
+    cmax = -1
+    for i in range(n):
+        li,ri,ci = map(int, input().split())
+        if i == 0:
+            print(ci)
+            cmin = min(li,cmin)
+            cmax = max(ri,cmax)
+        else:
+            
