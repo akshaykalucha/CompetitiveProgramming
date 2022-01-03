1fe7d50bcc04ddb04f1e9ff07ecc347c1577f8f1
akshay kalucha <akshaykalucha@gmail.com>
2022-01-03 22:18:25 +0530

    add

--- Codeforces/integershop.py
+++ Codeforces/integershop.py
@@ -5,11 +5,14 @@ for _ in range(int(input())):
     n = int(input())
     cmin = 9999999999
     cmax = -1
+    cost = []
     for i in range(n):
         li,ri,ci = map(int, input().split())
         if i == 0:
             print(ci)
             cmin = min(li,cmin)
             cmax = max(ri,cmax)
+            cost.sppend()
         else:
-            
+            if li<cmin or ri>cmax:
+                
