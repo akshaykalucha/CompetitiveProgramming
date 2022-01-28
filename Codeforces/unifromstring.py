2eb3db1e8569dea9d84febbc3548326f012eab59
Akshay Kalucha <akshay.kalucha@hypd.in>
2022-01-28 18:20:24 +0530

    add

--- Codeforces/unifromstring.py
+++ Codeforces/unifromstring.py
@@ -21,3 +21,79 @@ for _ in range(int(input())):
             else:
                 cc+=1
     print(ss)
+    for _ in range(int(input())):
+        n, k = map(int, input().split())
+    ss = ""
+    oo = 97
+    for i in range(k):
+        ss+=chr(oo)
+        if oo==122:
+            oo=1
+        else:
+            oo+=1
+    if n-k>0:
+        cc=0
+        for i in range(n-k):
+            ss+=ss[cc]
+            if cc==k-1:
+                cc=0
+            else:
+                cc+=1
+    print(ss)
+    for _ in range(int(input())):
+        n, k = map(int, input().split())
+    ss = ""
+    oo = 97
+    for i in range(k):
+        ss+=chr(oo)
+        if oo==122:
+            oo=1
+        else:
+            oo+=1
+    if n-k>0:
+        cc=0
+        for i in range(n-k):
+            ss+=ss[cc]
+            if cc==k-1:
+                cc=0
+            else:
+                cc+=1
+    print(ss)
+    for _ in range(int(input())):
+        n, k = map(int, input().split())
+    ss = ""
+    oo = 97
+    for i in range(k):
+        ss+=chr(oo)
+        if oo==122:
+            oo=1
+        else:
+            oo+=1
+    if n-k>0:
+        cc=0
+        for i in range(n-k):
+            ss+=ss[cc]
+            if cc==k-1:
+                cc=0
+            else:
+                cc+=1
+    print(ss)
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    ss = ""
+    oo = 97
+    for i in range(k):
+        ss+=chr(oo)
+        if oo==122:
+            oo=1
+        else:
+            oo+=1
+    if n-k>0:
+        cc=0
+        for i in range(n-k):
+            ss+=ss[cc]
+            if cc==k-1:
+                cc=0
+            else:
+                cc+=1
+    print(ss)
