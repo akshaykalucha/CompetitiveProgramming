8be673860110469f9dffa54476ac9eb9129c56e8
akshaykalucha3 <akshaykalucha@gmail.com>
2022-02-03 22:15:21 +0530

    commit from mac

--- Codeforces/boardgame.py
+++ Codeforces/boardgame.py
@@ -1,6 +1,6 @@
-import sys
-sys.stdout = open('DSA/Stacks/output.txt', 'w')
-sys.stdin = open('DSA/Stacks/input.txt', 'r')
+# import sys
+# sys.stdout = open('DSA/Stacks/output.txt', 'w')
+# sys.stdin = open('DSA/Stacks/input.txt', 'r')
 
 n = int(input())
 ll = list(map(int, input().split()))
